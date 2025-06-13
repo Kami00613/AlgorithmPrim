5947de5427ac0ebb058c42a9937809b65dd42dda
Kami00613 <shakurova.06kamila@gmail.com>
2025-06-13 09:56:16 +0300

    Update FileReader.java

--- src/main/java/PrimAlgorithm/FileReader.java
+++ src/main/java/PrimAlgorithm/FileReader.java
@@ -1,4 +1,34 @@
 package PrimAlgorithm;
 
+import java.io.*;
+import java.util.*;
+
 public class FileReader {
+    public static int getVertexCount(String filename) throws FileNotFoundException {
+        try (Scanner scanner = new Scanner(new File(filename))) {
+            if (scanner.hasNextInt()) {
+                return scanner.nextInt();
+            }
+        }
+        return 0;
+    }
+
+    public static List<PrimAlgorithm.Edge> readEdges(String filename) throws IOException {
+        List<PrimAlgorithm.Edge> edges = new ArrayList<>();
+        BufferedReader reader = new BufferedReader(new java.io.FileReader(filename));
+
+        reader.readLine(); // пропускаем первую строку
+
+        String line;
+        while ((line = reader.readLine()) != null && !line.trim().isEmpty()) {
+            String[] parts = line.split(" ");
+            int from = Integer.parseInt(parts[0]);
+            int to = Integer.parseInt(parts[1]);
+            int weight = Integer.parseInt(parts[2]);
+            edges.add(new PrimAlgorithm.Edge(from, to, weight));
+        }
+
+        reader.close();
+        return edges;
+    }
 }
